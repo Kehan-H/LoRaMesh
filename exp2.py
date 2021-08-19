7dbec098d56e8930ad414142c43287ccead94258
Kehan Huang <daisukihatsune@gmail.com>
2021-08-20 02:47:59 +1000

    temp

--- exp2.py
+++ exp2.py
@@ -14,7 +14,7 @@ import reporting as rp
 
 # simulation settings
 simtime = 5*1000*60*60
-#random.seed(15)
+random.seed(15)
 
 # network settings
 nw.EXP = 2
@@ -30,13 +30,14 @@ nw.TTL = 10
 # protocol settings
 pr.n0 = 5
 pr.RM1 = 5
-pr.RM2 = 10
+pr.RM2 = 5
 pr.K = 5*60*1000
 pr.HL = 3
 
 # base station initialization
 locsB = np.array([397.188492418693,226.186250701973])
 gw = nw.myNode(0,locsB[0],locsB[1])
+gw.genPacket(0,25,1)
 gw.genPacket(0,25,1)
 nw.nodes.append(gw)
 
